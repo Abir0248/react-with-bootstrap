54ff9e2ae294b2f1d67de5143ff7ebeca154271c
Abir0248 <abir15-6789@diu.edu.bd>
2022-03-28 21:49:24 +0600

    cardgroup

--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Card from "../Card/Card";
 
 const CardGroup = () => {
+  const items = [
+    { id: 1, name: "Laptop", price: 55000 },
+    { id: 2, name: "Iphone", price: 75000 },
+    { id: 3, name: "Smart Watch", price: 15000 },
+  ];
   return (
     <div>
-      <h1>This is card</h1>
+      <h1>This is cards</h1>
+      <div className="card-group">
+        {items.map((item) => (
+          <Card></Card>
+        ))}
+      </div>
     </div>
   );
 };
